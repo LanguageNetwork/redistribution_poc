b3267c6afd3d7b2121cbef94764dc285dcc2c73f
kcrong <iam@devkcr.org>
2018-03-09 14:13:12 +0900

    Add creation test

--- test/TestNodeRelationship.sol
+++ test/TestNodeRelationship.sol
@@ -7,8 +7,13 @@ import "truffle/DeployedAddresses.sol";
 // Our contract
 import "../contracts/storage_patterns/NodeRelationship.sol";
 
+// Type import
+
 
 contract TestNodeRelationship {
+    bytes32 testDataSetId = "test_dataset_id";
+    bytes32 testRawDataId = "test_raw_data_id";
+
     event Logging(address asdf);
 
     function testTruffleTest() public {
@@ -17,9 +22,57 @@ contract TestNodeRelationship {
         Assert.equal(1000, expected, "Comparing Integer");
     }
 
-    function testInitialWallet() public {
+    function testConstructor() public {
         NodeRelationship node = NodeRelationship(DeployedAddresses.NodeRelationship());
+        address value = node.owner();
+        address expected = tx.origin;
+
+        Assert.equal(value, expected, "Constructor Testing");
 
     }
 
+    function testGetDataSetCount() public {
+        NodeRelationship node = new NodeRelationship();
+        uint value = node.getDataSetCount();
+        uint expected = 0;
+
+        Assert.equal(value, expected, "getDataSetCount() should return 0 at start");
+    }
+
+    function testGetRawDataCount() public {
+        NodeRelationship node = new NodeRelationship();
+        uint value = node.getRawDataCount();
+        uint expected = 0;
+
+        Assert.equal(value, expected, "getRawDataCount() should return 0 at start");
+    }
+
+    function testAddDataSet() public {
+        NodeRelationship node = new NodeRelationship();
+        bool result;
+
+        result = node.createDataSet(testDataSetId);
+        Assert.equal(result, true, "createDataSet() should return true");
+
+        result = node.isDataSet(testDataSetId);
+        Assert.equal(result, true, "isDataSet() should return true with valid dataset id");
+
+        result = node.isDataSet("wrong_dataset_id");
+        Assert.equal(result, false, "isDataSet() should return false with invalid dataset id");
+    }
+    
+    function testAddRawData() public {
+        NodeRelationship node = new NodeRelationship();
+        bool result;
+        
+        result = node.createRawData(testRawDataId);
+        Assert.equal(result, true, "createRawData() should return true");
+
+        result = node.isRawData(testRawDataId);
+        Assert.equal(result, true, "isRawData() should return true with valid dataset id");
+
+        result = node.isRawData("wrong_dataset_id");
+        Assert.equal(result, false, "isRawData() should return false with invalid dataset id");
+        
+    }
 }
